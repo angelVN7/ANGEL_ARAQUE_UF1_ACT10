3f5a884f714a6fef0ff0dd2c6ff3fff7ec17674d
INFO1-EQUIP10\Tarda <a23angarator@inspedralbes.cat>
2024-10-29 20:45:41 +0100

    Ej10 movail

---
+++ ANGEL_ARAQUE_UF1_ACT10/app/src/main/java/com/example/angel_araque_uf1_act10/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.angel_araque_uf1_act10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val registrarse:Button = findViewById(R.id.registro)
+        val nombre:EditText = findViewById(R.id.nombre)
+        val apellido:EditText = findViewById(R.id.apellido)
+        val edad:EditText = findViewById(R.id.edad)
+        val usuario:EditText = findViewById(R.id.usuario)
+        val cotrasenya:EditText = findViewById(R.id.contrasenya)
+
+        registrarse.setOnClickListener {
+            if(nombre.text.isEmpty() || apellido.text.isEmpty() || edad.text.isEmpty() || usuario.text.isEmpty() || cotrasenya.text.isEmpty())
+                Toast.makeText(this,"No se pudo completar el registro rellene todos los campos",Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(this,"Registro completado",Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
---
+++ ANGEL_ARAQUE_UF1_ACT10/app/src/main/java/com/example/angel_araque_uf1_act10/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.angel_araque_uf1_act10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
